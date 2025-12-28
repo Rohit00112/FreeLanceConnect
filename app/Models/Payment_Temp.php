105b13c852de950214922e7178fb4a748de47c2f
rohit00112 <subedirohit49@gmail.com>
2025-12-28 21:03:22 +0545

    Step 1: Temp rename for case sensitivity

--- app/Models/Payment_Temp.php
+++ app/Models/Payment_Temp.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class payment extends Model
+class Payment extends Model
 {
      protected $fillable = [
           'transaction_id',
